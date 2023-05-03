ad197d1a9f8ee34dc72ce960b276e2e40fee5d99
Vladislav <tokenvlad@gmail.com>
2023-05-03 11:43:24 +0200

    ArraysAndObjs

---
+++ src/ArrayAndObj/Second.java
@@ -0,0 +1,15 @@
+package ArrayAndObj;
+
+public class Second {
+    // Field is an object variable of class First:
+    First obj;
+    // Class constructor:
+    public Second(int n) {
+        // Create an object and bind it with field - object variable
+    obj = new First (n);
+    obj.show();
+        // Displaying the contents of the field - object:
+
+
+    }
+}
